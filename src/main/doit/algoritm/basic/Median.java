bca793cd1bde9b4d765a5a7c8a2e8564c4ecffdf
raoul <raoul@snaps.com>
2020-08-03 19:25:57 +0900

    initial commit

---
+++ src/main/doit/algoritm/basic/Median.java
@@ -0,0 +1,14 @@
+package main.doit.algoritm.basic;
+
+public class Median {
+
+    /*static int med3(int a, int b, int c) {
+        if(a >= b) {
+            if(b >= c) {
+                return b;
+            } else if (a <= c) {
+                return a;
+            }
+        }
+    }*/
+}
